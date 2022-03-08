2fc8897c556610afb3743c31f666ea57cf37a8ab
Nicola Menolascina <nicola.menola1505@gmail.com>
2022-03-08 22:08:49 +0100

    Modifiche varie

--- Publish.cs
+++ Publish.cs
@@ -11,82 +11,85 @@ namespace MQTTApp
         {
             try
             {
-                Console.WriteLine("Starting Subscriber...");
-                //Creazione Client
-                var factory = new MqttFactory();
-                var _Client = factory.CreateMqttClient();
-                string clientid = Guid.NewGuid().ToString();
-
-                Console.WriteLine("Confiruando Opzioni...");
-                //Configurazione Opzioni
-                var _options = new MqttClientOptionsBuilder()
-                    .WithClientId(clientid)
-                    .WithTcpServer("broker.mqttdashboard.com", 1883)
-                    .WithCredentials("Gabriele", "Squeo")
-                    .WithCleanSession()
-                    .Build();
-
-                //Connessione
-                _Client.ConnectAsync(_options).Wait();
-
-                if (_Client.IsConnected)
+                while (true)
                 {
-                    Console.WriteLine("1)Pubblica:\n2)Ricevi:");
-                    string input = Console.ReadLine();
-                    if (input == "1")
+                    Console.WriteLine("Starting Subscriber...");
+                    //Creazione Client
+                    var factory = new MqttFactory();
+                    var _Client = factory.CreateMqttClient();
+                    string clientid = Guid.NewGuid().ToString();
+
+                    Console.WriteLine("Confiruando Opzioni...");
+                    //Configurazione Opzioni
+                    var _options = new MqttClientOptionsBuilder()
+                        .WithClientId(clientid)
+                        .WithTcpServer("broker.mqttdashboard.com", 1883)
+                        .WithCredentials("Gabriele", "Squeo")
+                        .WithCleanSession()
+                        .Build();
+
+                    //Connessione
+                    _Client.ConnectAsync(_options).Wait();
+
+                    if (_Client.IsConnected)
                     {
-                        Console.WriteLine("Pubblicando Messaggio...");
+                        Console.WriteLine("1)Pubblica:\n2)Ricevi:");
+                        string input = Console.ReadLine();
+                        if (input == "1")
+                        {
+                            Console.WriteLine("Inserisci testo da pubblicare");
+                            String testo = Console.ReadLine();
 
-                        //Pubblicazione Messaggio
-                        var applicationMessage = new MqttApplicationMessageBuilder()
-                           .WithTopic("panetti/test")
-                           .WithPayload("Ciao a tutti")
-                           .Build();
+                            //Pubblicazione Messaggio
+                            var applicationMessage = new MqttApplicationMessageBuilder()
+                               .WithTopic("panetti/test")
+                               .WithPayload(testo)
+                               .Build();
 
-                        _Client.PublishAsync(applicationMessage, CancellationToken.None);
+                            _Client.PublishAsync(applicationMessage, CancellationToken.None);
 
-                        Console.WriteLine("MQTT application message is published.");
+                            Console.WriteLine("MQTT application message is published.");
+                        }
+                        else
+                        {
+                            //Sottoscrizione
+
+                            var mqttSubscribeOptions = factory.CreateSubscribeOptionsBuilder()
+                               .WithTopicFilter(f => { f.WithTopic("panetti/test"); })
+                               .Build();
+
+                            _Client.SubscribeAsync(mqttSubscribeOptions).Wait();
+
+                            //Lettura Messaggi
+                            while (true)
+                            {
+                                _Client.UseApplicationMessageReceivedHandler(e =>
+                                {
+                                    try
+                                    {
+                                        string topic = e.ApplicationMessage.Topic;
+
+                                        if (string.IsNullOrWhiteSpace(topic) == false)
+                                        {
+                                            string payload = Encoding.UTF8.GetString(e.ApplicationMessage.Payload);
+                                            Console.WriteLine($"Topic1: {topic}. Message Received: {payload}");
+                                        }
+                                    }
+                                    catch (Exception ex)
+                                    {
+                                        Console.WriteLine(ex.Message, ex);
+                                    }
+                                });
+                            }
+
+                        }
+
                     }
                     else
                     {
-                        //Sottoscrizione
-
-                        var mqttSubscribeOptions = factory.CreateSubscribeOptionsBuilder()
-                           .WithTopicFilter(f => { f.WithTopic("panetti/test"); })
-                           .Build();
-
-                        _Client.SubscribeAsync(mqttSubscribeOptions).Wait();
-                        
-                        //Lettura Messaggi
-                        while (true)
-                        {
-                            _Client.UseApplicationMessageReceivedHandler(e =>
-                            {
-                                try
-                                {
-                                    string topic = e.ApplicationMessage.Topic;
-
-                                    if (string.IsNullOrWhiteSpace(topic) == false)
-                                    {
-                                        string payload = Encoding.UTF8.GetString(e.ApplicationMessage.Payload);
-                                        Console.WriteLine($"Topic1: {topic}. Message Received: {payload}");
-                                    }
-                                }
-                                catch (Exception ex)
-                                {
-                                    Console.WriteLine(ex.Message, ex);
-                                }
-                            });
-                        }
-                        
+                        Console.WriteLine("Non connesso.");
                     }
-                    
                 }
-                else
-                {
-                    Console.WriteLine("Non connesso.");
-                }
-                
             }
             catch (Exception e)
             {
